0a1565c1077c5c2e9affdb848a4cc75173bc96f6
SH4ND <miqueiasantos20@gmail.com>
2023-02-21 10:56:12 -0300

    jeter: Initial changes & support retrofit

--- device.mk
+++ device.mk
@@ -6,3 +6,15 @@
 #
 
 LOCAL_PATH := device/motorola/jeter
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
+
+# Ramdisk
+BOARD_RAMDISK_USE_LZMA := true
+LZMA_RAMDISK_TARGETS := recovery
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
